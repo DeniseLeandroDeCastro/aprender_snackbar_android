76ee98a638dc42a43e0af68a3aeb0e53b69e40e7
Denise <dmaria.natal@gmail.com>
2021-03-06 10:51:00 -0300

    aprender a usar snackbar no android

---
+++ app/src/main/java/br/edu/ifrn/snackbar/MainActivity.java
@@ -0,0 +1,40 @@
+package br.edu.ifrn.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAbrir = findViewById(R.id.buttonAbrir);
+
+        //Configurando o click do botão
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(
+                        view, "Botão pressionado",
+                        Snackbar.LENGTH_LONG
+                ).setAction("Confirmar", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonAbrir.setText("Botão abrir alterado");
+                    }
+                }).setActionTextColor(getResources()
+                        .getColor(R.color.teal_200)).show();
+            }
+        });
+    }
+}
